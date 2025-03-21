7e0627fddb6215d9d39c613e1f50d8a3026ae6e7
sudevmagar <sudebm789@gmail.com>
2025-03-21 19:17:51 +0545

    fix image

--- app/components/DoctorsSection.js
+++ app/components/DoctorsSection.js
@@ -48,8 +48,8 @@ export default function DoctorsSection() {
             <div className="flex justify-center">
               <Image
                 src={`/image/${doctor.img}`}
-                height={0}
-                width={0}
+                height={100}
+                width={100}
                 alt={doctor.name}
                 className="h-40 w-40 rounded-md border-2 border-[#16a085] my-4 object-cover"
               />
